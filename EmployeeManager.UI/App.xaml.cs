b0b0b25b15dbbd3a344680482d0294a8282e32f8
Christian Khalil <christian.khalil2000@gmail.com>
2021-01-13 00:09:10 +0100

    bug?

--- EmployeeManager.UI/App.xaml.cs
+++ EmployeeManager.UI/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using EmployeeManager.DataRepository.Services;
+using EmployeeManager.UI.ViewModels;
+using EmployeeManager.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 
 namespace EmployeeManager
 {
@@ -7,5 +11,19 @@ namespace EmployeeManager
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<MainWindowViewModel>();
+
+            var provider = services.BuildServiceProvider();
+            var mainWindow = provider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
--- EmployeeManager.UI/Views/MainWindow.xaml.cs
+++ EmployeeManager.UI/Views/MainWindow.xaml.cs
@@ -1,20 +1,14 @@
 ﻿using EmployeeManager.UI.ViewModels;
-using EmployeeManager.UI;
 using System.Windows;
-using EmployeeManager.UI.DI;
-using Autofac;
-using EmployeeManager.DataRepository.Services;
 
 namespace EmployeeManager.Views
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowViewModel mainWindowViewModel)
         {
-            InitializeComponent();
-            var container = ContainerConfig.Configure();
-            var start = container.Resolve<AppDbContext>();
-            DataContext = new MainWindowViewModel(start);
+            InitializeComponent();          
+            DataContext = mainWindowViewModel;
         }
     }
 }
